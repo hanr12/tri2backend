d8522d77279da8081d74c6446929266e8c647ec1
isabellehp <izzypark@icloud.com>
2024-02-11 18:33:44 -0800

    edits to user and main py

--- model/users.py
+++ model/users.py
@@ -75,19 +75,19 @@ class User(db.Model):
     # Define the User schema with "vars" from object
     id = db.Column(db.Integer, primary_key=True)
     _name = db.Column(db.String(255), unique=False, nullable=False)
+    _age = db.Column(db.String(255), unique=True, nullable=False)
     _uid = db.Column(db.String(255), unique=True, nullable=False)
     _password = db.Column(db.String(255), unique=False, nullable=False)
-    _dob = db.Column(db.Date)
     
     # Defines a relationship between User record and Notes table, one-to-many (one user to many notes)
     posts = db.relationship("Post", cascade='all, delete', backref='users', lazy=True)
 
     # constructor of a User object, initializes the instance variables within object (self)
-    def __init__(self, name, uid, password="123qwerty", dob=date.today()):
+    def __init__(self, name, age, uid, password="123qwerty"):
         self._name = name    # variables with self prefix become part of the object, 
+        self._age = age
         self._uid = uid
         self.set_password(password)
-        self._dob = dob
 
     # a name getter method, extracts name from object
     @property
@@ -98,6 +98,15 @@ class User(db.Model):
     @name.setter
     def name(self, name):
         self._name = name
+
+# an age getter method, extracts age from object
+    @property
+    def age(self):
+        return self._age
+    
+    @age.setter
+    def age(self, age):
+        self._age = age
     
     # a getter method, extracts email from object
     @property
@@ -128,21 +137,21 @@ class User(db.Model):
         result = check_password_hash(self._password, password)
         return result
     
-    # dob property is returned as string, to avoid unfriendly outcomes
-    @property
-    def dob(self):
-        dob_string = self._dob.strftime('%m-%d-%Y')
-        return dob_string
+    # # dob property is returned as string, to avoid unfriendly outcomes
+    # @property
+    # def dob(self):
+    #     dob_string = self._dob.strftime('%m-%d-%Y')
+    #     return dob_string
     
-    # dob should be have verification for type date
-    @dob.setter
-    def dob(self, dob):
-        self._dob = dob
+    # # dob should be have verification for type date
+    # @dob.setter
+    # def dob(self, dob):
+    #     self._dob = dob
     
-    @property
-    def age(self):
-        today = date.today()
-        return today.year - self._dob.year - ((today.month, today.day) < (self._dob.month, self._dob.day))
+    # @property
+    # def age(self):
+    #     today = date.today()
+    #     return today.year - self._dob.year - ((today.month, today.day) < (self._dob.month, self._dob.day))
     
     # output content using str(object) in human readable form, uses getter
     # output content using json dumps, this is ready for API response
@@ -167,23 +176,23 @@ class User(db.Model):
         return {
             "id": self.id,
             "name": self.name,
-            "uid": self.uid,
-            "dob": self.dob,
             "age": self.age,
+            "uid": self.uid,
             "posts": [post.read() for post in self.posts]
         }
 
     # CRUD update: updates user name, password, phone
     # returns self
-    def update(self, name="", uid="", password=""):
+    def update(self, name="", age="", uid="", password=""):
         """only updates values with length"""
         if len(name) > 0:
             self.name = name
+        if len(age) > 0:
+            self.age = age
         if len(uid) > 0:
             self.uid = uid
         if len(password) > 0:
             self.set_password(password)
-        db.session.commit()
         return self
 
     # CRUD delete: remove self
@@ -203,10 +212,10 @@ def initUsers():
         """Create database and tables"""
         db.create_all()
         """Tester data for table"""
-        u1 = User(name='Thomas Edison', uid='toby', password='123toby', dob=date(1847, 2, 11))
-        u2 = User(name='Nicholas Tesla', uid='niko', password='123niko', dob=date(1856, 7, 10))
-        u3 = User(name='Alexander Graham Bell', uid='lex')
-        u4 = User(name='Grace Hopper', uid='hop', password='123hop', dob=date(1906, 12, 9))
+        u1 = User(name='Thomas Edison', age='176', uid='toby', password='123toby')
+        u2 = User(name='Nicholas Tesla', age='4', uid='niko', password='123niko')
+        u3 = User(name='Alexander Graham Bell', age='37', uid='lex', password='1agb')
+        u4 = User(name='Grace Hopper', age='61', uid='hop', password='123hop')
         users = [u1, u2, u3, u4]
 
         """Builds sample user/note(s) data"""
