87708d2fe23cf81c637007d8ac0ebd3460d28a19
isabellehp <izzypark@icloud.com>
2024-03-07 20:28:21 -0800

    edit

--- model/users.py
+++ model/users.py
@@ -218,10 +218,10 @@ def initUsers():
         """Create database and tables"""
         db.create_all()
         """Tester data for table"""
-        u1 = User(name='Thomas Edison', uid='toby', password='123toby', dob=date(1847, 2, 11))
-        u2 = User(name='Nicholas Tesla', uid='niko', password='123niko', dob=date(1856, 7, 10))
-        u3 = User(name='Alexander Graham Bell', uid='lex')
-        u4 = User(name='Grace Hopper', uid='hop', password='123hop', dob=date(1906, 12, 9))
+        u1 = User(name='Thomas Edison', uid='123toby', password='123toby', dob=date(1847, 2, 11))
+        u2 = User(name='Nicholas Tesla', uid='123niko', password='123niko', dob=date(1856, 7, 10))
+        u3 = User(name='Alexander Graham Bell', uid='123lex')
+        u4 = User(name='Grace Hopper', uid='123hop', password='123hop', dob=date(1906, 12, 9))
         users = [u1, u2, u3, u4]
 
         """Builds sample user/note(s) data"""
